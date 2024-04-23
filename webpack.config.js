5f3569d386bf7886afe0e7a9d8b7f28b7374013c
Gregory Jung <tenor2000@gmail.com>
2024-04-23 11:30:27 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+        },
+        {
+            test: /\.(csv|tsv)$/i,
+            use: ['csv-loader'],
+        },
+        {
+            test: /\.xml$/i,
+            use: ['xml-loader'],
+        },
+    ]
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Development',
+    }),
+  ]
+};
